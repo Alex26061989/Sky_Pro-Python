e5befc8108bf0f19819c54ebe107a83d17811f18
Alex26061989 <Xrustalev.061989@gmai.com>
2025-11-19 14:03:15 +0300

    Тренировка

---
+++ Тренировка/Тренировка_01.py
@@ -0,0 +1,23 @@
+my_heigh = 186
+print(my_heigh)
+
+my_name = "Алексей"
+my_name = "Алексей Хрусталёв"
+print(my_name)
+
+pet_name = input("Как зовут вашего питомца? ")
+print("Ваш любимчик " + pet_name)
+  
+def print_python():
+    print("Учу Python!")
+
+def print_letter(let):
+    print(let, end='')
+
+print_letter('S')
+print_letter('k')
+print_letter('y')
+print_letter('P')
+print_letter('r')
+print_letter('o')
+
